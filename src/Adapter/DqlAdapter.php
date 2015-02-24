2eac01ec3553b6c58b93180c0a6f8a914353859c
Kristen Gilden <kristen.gilden@gmail.com>
2015-02-24 16:55:17 +0200

    Create DqlAdapter (first adapter \o/)

---
+++ src/Adapter/DqlAdapter.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the Pager package.
+ *
+ * (c) Kristen Gilden kristen.gilden@gmail.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KG\Pager\Adapter;
+
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use KG\Pager\AdapterInterface;
+
+/**
+ * Enables paging of Doctrine ORM's queries. Usage:
+ *
+ *     // Works with both Query & QueryBuilder instances.
+ *     $pager->paginate(DqlAdapter::fromQuery($query));
+ *
+ *     // Or simply wrap Query & QueryBuilder instances in a Paginator object.
+ *     $pager->paginate(new DqlAdapter(new Paginator($query)));
+ *
+ * Be careful with using this adapter though: performance is abysmal for
+ * sufficiently complex queries.
+ */
+final class DqlAdapter implements AdapterInterface
+{
+    /**
+     * @var Paginator
+     */
+    private $paginator;
+
+    /**
+     * @param Paginator $paginator
+     */
+    public function __construct(Paginator $paginator)
+    {
+        $this->paginator = $paginator;
+    }
+
+    /**
+     * @see Paginator
+     *
+     * @param Doctrine\ORM\Query|Doctrine\ORM\QueryBuilder $query               A Doctrine ORM query or query builder.
+     * @param boolean                                      $fetchJoinCollection Whether the query joins a collection (true by default).
+     *
+     * @return SimpleDqlAdapter
+     */
+    public static function fromQuery($query, $fetchJoinCollection = true)
+    {
+        return new static(new Paginator($query, $fetchJoinCollection));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getItemCount()
+    {
+        return $this->paginator->count();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getItems($offset, $limit)
+    {
+        $this
+            ->paginator
+            ->getQuery()
+            ->setFirstResult($offset)
+            ->setMaxResults($limit)
+        ;
+
+        return $this->paginator->getIterator();
+    }
+}
---
+++ tests/Adapter/DqlAdapterTest.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ * This file is part of the Pager package.
+ *
+ * (c) Kristen Gilden kristen.gilden@gmail.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KG\Pager\Tests\Adapter;
+
+use KG\Pager\Adapter\DqlAdapter;
+
+class DqlAdapterTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        if (!class_exists('Doctrine\ORM\Query')) {
+            $this->markTestSkipped('doctrine/orm must be installed to run this test');
+        }
+    }
+
+    public function testgetItemCountDelegatesToCount()
+    {
+        $paginator = $this->getMockPaginator();
+
+        $paginator
+            ->expects($this->once())
+            ->method('count')
+            ->willReturn(5)
+        ;
+
+        $adapter = new DqlAdapter($paginator);
+        $this->assertEquals(5, $adapter->getItemCount());
+    }
+
+    public function testGetItemsDelegatesToGetIterator()
+    {
+        $query = $this->getMockQuery();
+
+        $query
+            ->expects($this->once())
+            ->method('setFirstResult')
+            ->with(10)
+            ->will($this->returnSelf())
+        ;
+
+        $query
+            ->expects($this->once())
+            ->method('setMaxResults')
+            ->with(2)
+            ->will($this->returnSelf())
+        ;
+
+        $paginator = $this->getMockPaginator();
+        $paginator->method('getQuery')->willReturn($query);
+
+        $paginator
+            ->expects($this->once())
+            ->method('getIterator')
+            ->willReturn($expected = new \ArrayIterator(array('foo', 'bar')))
+        ;
+
+        $adapter = new DqlAdapter($paginator);
+        $this->assertSame($expected, $adapter->getItems(10, 2));
+    }
+
+    private function getMockPaginator()
+    {
+        return $this
+            ->getMockBuilder('Doctrine\ORM\Tools\Pagination\Paginator')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+    }
+
+    private function getMockQuery()
+    {
+        return $this
+            ->getMockBuilder('\Doctrine\ORM\AbstractQuery')
+            ->setMethods(array('setParameter', 'getResult', 'getQuery', 'setFirstResult', 'setMaxResults'))
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass()
+        ;
+    }
+}
